d0f38e8b985f67e764143b58a1718ed6ffdcdbaf
Jan Steinhoff <jan.steinhoff@sinnerschrader.com>
2019-03-02 15:06:27 +0100

    work around locale prefix issues

--- src/components/LanguageSelect.tsx
+++ src/components/LanguageSelect.tsx
@@ -22,7 +22,9 @@ const LanguageSwitch: React.SFC<LanguageSwitchProps> = ({
 					className={
 						language === locale ? styles.switchBtnActive : styles.switchBtn
 					}
-					onClick={() => navigate(language === 'de' ? '/' : '/en/')}
+					onClick={() =>
+						navigate(language === 'de' ? '/' : 'selected-gadgets/en/')
+					}
 				>
 					{language}
 				</button>
