a82f4f92bb47c63ad3b3cf8f9f8318220af2ceee
Jan Steinhoff <jan.steinhoff@sinnerschrader.com>
2019-03-03 19:04:21 +0100

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-7c8d451f76a74398f7a1.js"
+    "url": "webpack-runtime-0b390e4719fc7e358afe.js"
   },
   {
     "url": "app.b6413b9397877d632b60.css"
@@ -39,7 +39,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "274f11f6122a1285055070e100e9bbf6"
+    "revision": "2eabcf904553556ae431d3773d56f8fb"
   },
   {
     "url": "static/d/229/path---offline-plugin-app-shell-fallback-a-30-c5a-5l4NDdIVGDoNQAdpYpS09f1lya4.json"
